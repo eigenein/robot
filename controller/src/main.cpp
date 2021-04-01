c438815002c6ae44aa0855d2ddd40102cf5ec29d
Pavel Perestoronin <eigenein@gmail.com>
2021-04-01 21:17:41 +0200

    💥 Integrate BNO055 absolute position sensor

--- controller/src/main.cpp
+++ controller/src/main.cpp
@@ -1,8 +1,10 @@
+#include <math.h>
+
 #include <Arduino.h>
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_HMC5883_U.h>
+#include <Adafruit_BNO055.h>
 #include <Ticker.h>
 
 // -------------------------------------------------------------------------------------------------
@@ -10,29 +12,10 @@
 // -------------------------------------------------------------------------------------------------
 
 static const unsigned int PRINT_TELEMETRY_INTERVAL_MILLIS = 500;
-static const unsigned int READ_COMPASS_INTERVAL_MILLIS = 1000 / 15; // Default readings rate is 15Hz.
 static const unsigned int READ_CONSOLE_INTERVAL_MILLIS = 100;
+static const unsigned int READ_POSITION_SENSOR_INTERVAL_MILLIS = 50;
+
 static const unsigned int MAX_CONSOLE_INPUT_LENGTH = 40;
-
-static const float COMPASS_SMOOTHING_FACTOR = 0.3f;
-
-// -------------------------------------------------------------------------------------------------
-// Custom types.
-// -------------------------------------------------------------------------------------------------
-
-// TODO: we won't need this. Instead, just set speed of each motor.
-typedef enum MotorMode {
-    FORWARD,
-    BACKWARD,
-    OFF,
-    SHORT_BREAK,
-};
-
-typedef struct MotorPins {
-    unsigned int pin1;
-    unsigned int pin2;
-    unsigned int pwm;
-};
 
 // -------------------------------------------------------------------------------------------------
 // Peripherals and pins.
@@ -45,15 +28,13 @@ static const unsigned int PIN_MOTOR_RIGHT_1 = 4;
 static const unsigned int PIN_MOTOR_RIGHT_2 = 2;
 static const unsigned int PIN_MOTOR_RIGHT_PWM = 9;
 
-static const unsigned int PIN_MOTOR_LEFT_1 = 7;
-static const unsigned int PIN_MOTOR_LEFT_2 = 8;
+static const unsigned int PIN_MOTOR_LEFT_1 = 8;
+static const unsigned int PIN_MOTOR_LEFT_2 = 7;
 static const unsigned int PIN_MOTOR_LEFT_PWM = 10;
 
-static const MotorPins PINS_MOTOR_RIGHT = {PIN_MOTOR_RIGHT_1, PIN_MOTOR_RIGHT_2, PIN_MOTOR_RIGHT_PWM};
-static const MotorPins PINS_MOTOR_LEFT = {PIN_MOTOR_LEFT_1, PIN_MOTOR_LEFT_2, PIN_MOTOR_LEFT_PWM};
+static const unsigned int PIN_BUZZER = 17;
 
-// TODO: replace with `Adafruit_BNO055`.
-Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified();
+static Adafruit_BNO055 positionSensor = Adafruit_BNO055(-1, 0x29);
 
 // -------------------------------------------------------------------------------------------------
 // Forward declarations.
@@ -61,19 +42,22 @@ Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified();
 
 void initializeSerial();
 void initializePins();
-void initializeCompass();
+void initializePositionSensor();
 void startTickers();
 
 void printTelemetry();
 void readCompass();
 void readConsole();
+void readPositionSensor();
 
-void incrementRotaryLeft();
-void incrementRotaryRight();
+void onLeftRotaryChange();
+void onRightRotaryChange();
 
-void controlMotor(const MotorPins&, const MotorMode, const unsigned int);
+void controlMotor(const unsigned int, const unsigned int, const unsigned int, const int);
+void controlLeftMotor(const int);
+void controlRightMotor(const int);
 
-void handleConsoleInput(const char[], unsigned int);
+void handleConsoleInput(const char[], const unsigned int);
 
 // -------------------------------------------------------------------------------------------------
 // Tickers.
@@ -81,8 +65,8 @@ void handleConsoleInput(const char[], unsigned int);
 // -------------------------------------------------------------------------------------------------
 
 Ticker printTelemetryTicker(printTelemetry, PRINT_TELEMETRY_INTERVAL_MILLIS);
-Ticker readCompassTicker(readCompass, READ_COMPASS_INTERVAL_MILLIS);
 Ticker readConsoleTicker(readConsole, READ_CONSOLE_INTERVAL_MILLIS);
+Ticker readPositionSensorTicker(readPositionSensor, READ_POSITION_SENSOR_INTERVAL_MILLIS);
 
 // -------------------------------------------------------------------------------------------------
 // Current state.
@@ -90,12 +74,7 @@ Ticker readConsoleTicker(readConsole, READ_CONSOLE_INTERVAL_MILLIS);
 
 static bool isTelemetryEnabled = true;
 
-// TODO: replace with the BNO055 orientation data.
-static float compassX = 0.0f;
-static float compassY = 0.0f;
-
-static float positionTicksX = 0.0f;
-static float positionTicksY = 0.0f;
+static sensors_event_t orientation, acceleration;
 
 // -------------------------------------------------------------------------------------------------
 // Arduino setup and loop.
@@ -107,14 +86,19 @@ void setup() {
     Serial.println("└[∵]┘ Hi! Press <Enter> to enter the CLI.");
 
     initializePins();
-    initializeCompass();
+    initializePositionSensor();
     startTickers();
+
+    Serial.println("└[∵]┐ Initialization delay…");
+    delay(1000); // TODO: check what kind of delay is actually needed by BNO055.
+    Serial.println("└[∵]┘ Initialized.");
+    tone(PIN_BUZZER, 880, 50); // Initialization tone.
 }
 
 void loop() {
     printTelemetryTicker.update();
-    readCompassTicker.update();
     readConsoleTicker.update();
+    readPositionSensorTicker.update();
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -125,20 +109,20 @@ void initializePins() {
     pinMode(PIN_ROTARY_LEFT, INPUT);
     pinMode(PIN_ROTARY_RIGHT, INPUT);
 
-    attachInterrupt(digitalPinToInterrupt(PIN_ROTARY_LEFT), incrementRotaryLeft, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(PIN_ROTARY_RIGHT), incrementRotaryRight, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_ROTARY_LEFT), onLeftRotaryChange, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_ROTARY_RIGHT), onRightRotaryChange, CHANGE);
 
     pinMode(LED_BUILTIN, OUTPUT);
     
-    controlMotor(PINS_MOTOR_RIGHT, MotorMode::OFF, 0);
-    pinMode(PIN_MOTOR_RIGHT_1, OUTPUT);
-    pinMode(PIN_MOTOR_RIGHT_2, OUTPUT);
-    pinMode(PIN_MOTOR_RIGHT_PWM, OUTPUT);
-
-    controlMotor(PINS_MOTOR_LEFT, MotorMode::OFF, 0);
+    controlLeftMotor(0);
     pinMode(PIN_MOTOR_LEFT_1, OUTPUT);
     pinMode(PIN_MOTOR_LEFT_2, OUTPUT);
     pinMode(PIN_MOTOR_LEFT_PWM, OUTPUT);
+
+    controlRightMotor(0);
+    pinMode(PIN_MOTOR_RIGHT_1, OUTPUT);
+    pinMode(PIN_MOTOR_RIGHT_2, OUTPUT);
+    pinMode(PIN_MOTOR_RIGHT_PWM, OUTPUT);
 }
 
 void initializeSerial() {
@@ -146,17 +130,17 @@ void initializeSerial() {
     while (!Serial);
 }
 
-void initializeCompass() {
-    if (!compass.begin()) {
-        Serial.println("┌[∵]┐ Error: no HMC5883 detected.");
+void initializePositionSensor() {
+    if (!positionSensor.begin()) {
+        Serial.println("┌[∵]┐ Error: no BNO055 detected.");
         while (1);
     }
 }
 
 void startTickers() {
     printTelemetryTicker.start();
-    readCompassTicker.start();
     readConsoleTicker.start();
+    readPositionSensorTicker.start();
 }
 
 // -------------------------------------------------------------------------------------------------
@@ -168,17 +152,9 @@ void printTelemetry() {
         return;
     }
 
-    Serial.print("Compass: ");
-    Serial.print(compassX);
-    Serial.print(" ");
-    Serial.print(compassY);
-
-    Serial.print(" | ");
-
-    Serial.print("Position: ");
-    Serial.print(positionTicksX);
-    Serial.print(" ");
-    Serial.print(positionTicksY);
+    Serial.print("Direction: ");
+    Serial.print(orientation.orientation.x);
+    Serial.print(" deg");
 
     Serial.println();
 }
@@ -202,7 +178,8 @@ void readConsole() {
 }
 
 // Handle user console input and execute the command.
-void handleConsoleInput(const char input[], unsigned int length) {
+void handleConsoleInput(const char input[], const unsigned int length) {
+    int motorSpeed;
     if (length == 0) {
         isTelemetryEnabled = !isTelemetryEnabled;
         if (isTelemetryEnabled) {
@@ -210,14 +187,10 @@ void handleConsoleInput(const char input[], unsigned int length) {
         } else {
             Serial.println("└[∵]┘ Telemetry disabled. Entering CLI. Press <Enter> to quit.");
         }
-    } else if (strncasecmp(input, "start b", length) == 0) {
-        controlMotor(PINS_MOTOR_LEFT, MotorMode::FORWARD, 255);
-    } else if (strncasecmp(input, "reverse b", length) == 0) {
-        controlMotor(PINS_MOTOR_LEFT, MotorMode::BACKWARD, 255);
-    } else if (strncasecmp(input, "off b", length) == 0) {
-        controlMotor(PINS_MOTOR_LEFT, MotorMode::OFF, 0);
-    } else if (strncasecmp(input, "break b", length) == 0) {
-        controlMotor(PINS_MOTOR_LEFT, MotorMode::SHORT_BREAK, 255);
+    } else if (sscanf(input, "left %i", &motorSpeed) == 1) {
+        controlLeftMotor(motorSpeed);
+    } else if (sscanf(input, "right %i", &motorSpeed) == 1) {
+        controlRightMotor(motorSpeed);
     } else {
         Serial.print("┌[∵]┐ I don't understand: `");
         Serial.print(input);
@@ -228,64 +201,48 @@ void handleConsoleInput(const char input[], unsigned int length) {
     }
 }
 
-void readCompass() {
-    // Globally measured min's and max's:
-    static float minX = 0.0f, maxX = 0.0f, minY = 0.0f, maxY = 0.0f;
-    
-    // Here we keep the current filtered yet raw measurements:
-    static float filteredX = 0.0f;
-    static float filteredY = 0.0f;
-
-    sensors_event_t event; 
-    compass.getEvent(&event);
-
-    filteredX += (event.magnetic.x - filteredX) * COMPASS_SMOOTHING_FACTOR;
-    filteredY += (event.magnetic.y - filteredY) * COMPASS_SMOOTHING_FACTOR;
-
-    minX = min(minX, filteredX);
-    maxX = max(maxX, filteredX);
-    minY = min(minY, filteredY);
-    maxY = max(maxY, filteredY);
-
-    // Apply the offsets and normalize the magnetic vector:
-    float unnormalizedX = filteredX - (maxX + minX) / 2.0f;
-    float unnormalizedY = filteredY - (maxY + minY) / 2.0f;
-    float magnitude = sqrt(unnormalizedX * unnormalizedX + unnormalizedY * unnormalizedY);
-    compassX = unnormalizedX / magnitude;
-    compassY = unnormalizedY / magnitude;
+void onLeftRotaryChange() {
+    // TODO
 }
 
-void incrementRotaryLeft() {
-    // TODO: wheel rotation direction.
-    positionTicksX += 0.5f * compassX;
-    positionTicksY += 0.5f * compassY;
+void onRightRotaryChange() {
+    // TODO
 }
 
-void incrementRotaryRight() {
-    // TODO: wheel rotation direction.
-    positionTicksX += 0.5f * compassX;
-    positionTicksY += 0.5f * compassY;
-}
-
-// TODO: PID controller.
-void controlMotor(const MotorPins &pins, const MotorMode mode, const unsigned int speed) {
-    analogWrite(pins.pwm, speed);
-    switch (mode) {
-        case MotorMode::FORWARD:
-            digitalWrite(pins.pin2, LOW);
-            digitalWrite(pins.pin1, HIGH);
-            break;
-        case MotorMode::BACKWARD:
-            digitalWrite(pins.pin1, LOW);
-            digitalWrite(pins.pin2, HIGH);
-            break;
-        case MotorMode::OFF:
-            digitalWrite(pins.pin1, LOW);
-            digitalWrite(pins.pin2, LOW);
-            break;
-        case MotorMode::SHORT_BREAK:
-            digitalWrite(pins.pin1, HIGH);
-            digitalWrite(pins.pin2, HIGH);
-            break;
+void controlMotor(
+    const unsigned int pin1,
+    const unsigned int pin2,
+    const unsigned int pinPWM,
+    const int speed
+) {
+    analogWrite(pinPWM, abs(speed));
+    if (speed > 0) {
+        digitalWrite(pin1, LOW);
+        digitalWrite(pin2, HIGH);
+    } else if (speed < 0) {
+        digitalWrite(pin2, LOW);
+        digitalWrite(pin1, HIGH);
+    } else {
+        digitalWrite(pin1, HIGH);
+        digitalWrite(pin2, HIGH);
     }
+}
+
+void controlLeftMotor(const int speed) {
+    controlMotor(PIN_MOTOR_LEFT_1, PIN_MOTOR_LEFT_2, PIN_MOTOR_LEFT_PWM, speed);
+}
+
+void controlRightMotor(const int speed) {
+    controlMotor(PIN_MOTOR_RIGHT_1, PIN_MOTOR_RIGHT_2, PIN_MOTOR_RIGHT_PWM, speed);
+}
+
+void readPositionSensor() {
+    positionSensor.getEvent(&orientation, Adafruit_BNO055::VECTOR_EULER);
+    positionSensor.getEvent(&acceleration, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+    // Calculate the actual elapsed time.
+    static unsigned long lastReadingMicros = micros();
+    const unsigned long now = micros();
+    const double elapsed = (now - lastReadingMicros) / 1000000.0;
+    lastReadingMicros = now;
 }
