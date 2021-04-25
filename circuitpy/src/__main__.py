6083a2702965547b6cd367e1cf148abd28829467
Pavel Perestoronin <eigenein@gmail.com>
2021-04-25 18:34:01 +0200

    🚧 Testing HC-SR04

--- circuitpy/src/__main__.py
+++ circuitpy/src/__main__.py
@@ -6,25 +6,25 @@ from time import sleep
 import adafruit_logging
 
 from src.logging import CustomHandler
-from src.hcsr04 import HCSR04
+from src.hcsr04 import Hcsr04
 
 led = DigitalInOut(board.LED)
 led.direction = Direction.OUTPUT
 
 uart = UART(board.GP0, board.GP1, baudrate=9600)
 
-sonar = HCSR04(trigger_pin=board.GP12, echo_pin=board.GP13, timeout=0.5)
+sonar = Hcsr04(trigger_pin=board.GP12, echo_pin=board.GP13)
 
 logger = adafruit_logging.getLogger("main")
 logger.addHandler(CustomHandler(uart))
 logger.setLevel(adafruit_logging.DEBUG)
+logger.info("Initialized.")
 
 
 def main():
-    logger.info("Initialized.")
     while True:
         try:
-            logger.info(f"Hello from {sonar.distance:.1f}cm.")
-        except RuntimeError as e:
+            logger.info(f"Hello from {sonar.distance:.2f}m.")
+        except TimeoutError as e:
             logger.error(str(e))
         sleep(0.5)
--- circuitpy/src/hcsr04.py
+++ circuitpy/src/hcsr04.py
@@ -1,22 +1,41 @@
+"""
+HC-SR04 ultrasonic distance sensor.
+
+The Adafruit driver doesn't work for me on Pi Pico, thus wrote my own.
+"""
+
 from digitalio import DigitalInOut, Direction
 from microcontroller import Pin
-from time import sleep
+from pulseio import PulseIn
+from time import monotonic, sleep
 
 
-class HCSR04:
-    def __init__(self, *, trigger_pin: Pin, echo_pin: Pin, timeout=0.1):
+class Hcsr04:
+    def __init__(self, *, trigger_pin: Pin, echo_pin: Pin, timeout=1.0):
         self.trigger = DigitalInOut(trigger_pin)
         self.trigger.direction = Direction.OUTPUT
-        self.echo = DigitalInOut(echo_pin)
-        self.echo.direction = Direction.INPUT
+        self.trigger.value = False
+        self.echo_pin = echo_pin
+        self.echo = PulseIn(echo_pin, maxlen=1)
+        self.timeout = timeout
 
     @property
     def distance(self) -> float:
         """
         Measures the distance and returns it in metres.
         """
+        self.echo.clear()
+
+        # Issue the trigger pulse.
         self.trigger.value = True
-        sleep(0.00001)
+        sleep(0.000010)
         self.trigger.value = False
 
-        # TODO
+        # Wait for the echo pulse.
+        timeout_time = monotonic() + self.timeout
+        while not self.echo:
+            if monotonic() > timeout_time:
+                raise TimeoutError(f"Timed out while waiting for an echo.")
+
+        # Finally, convert the pulse duration to the distance.
+        return self.echo[0] * 0.00017
